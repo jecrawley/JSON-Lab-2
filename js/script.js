b903f5555880f2b18c48917b621e5bfad27d206e
James Crawley <jamescrawley@James-ZCW-15.local>
2016-09-30 15:01:20 -0400

    Lists all movie titles, year and companies of those made at the Golden Gate Bridge.

---
+++ js/script.js
@@ -0,0 +1,24 @@
+var movies = new XMLHttpRequest ();
+var display = document.getElementById("result");
+var movieData;
+var output = "";
+
+movies.open("GET", "https://data.sfgov.org/api/views/yitu-d5am/rows.json?accessType=DOWNLOAD", true);
+movies.send();
+
+movies.onreadystatechange = function () {
+    if (movies.readyState === 4 && movies.status === 200){
+        movieData = (JSON.parse(movies.responseText).data);
+        movieData.forEach(checkLocation);
+        display.innerHTML = output;
+    }
+}
+
+
+console.log(movieData);
+ function checkLocation (element, index, array) {
+     if (element[10] === "Golden Gate Bridge") {
+         output += "<p>Title: </p>" + element[8] + " <p>Release year:</p> " + element[9] + " <p>Production company: </p>" + element[12] + "</br>";
+     }
+
+ }
